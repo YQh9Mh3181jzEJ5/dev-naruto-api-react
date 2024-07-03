bf556094644ec5f95f845f9fcf983ae92dcb728b
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-07-03 19:29:42 +0900

    feat: add characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,79 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import dummyImage from "./assets/dummy.png";
 import "./App.css";
+interface Character {
+  id: number;
+  name: string;
+  images: string[];
+  debut?: {
+    anime?: string;
+    appearsIn?: string;
+    game?: string;
+    manga?: string;
+    movie?: string;
+    novel?: string;
+    ova?: string;
+  };
+  personal?: {
+    affiliation?: string;
+  };
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+    try {
+      setIsLoading(true);
+      const result = await axios.get(apiUrl);
+      setCharacters(result.data.characters);
+    } catch (error) {
+      setError("キャラクター情報の取得に失敗しました。");
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) return <div>読み込み中……</div>;
+  if (error) return <div>{error}</div>;
 
   return (
-    <>
-      <h1>ナルト図鑑！</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>ナルト図鑑です</p>
-      </div>
-      <p className="read-the-docs">ナルト図鑑です</p>
-    </>
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => {
+            return (
+              <div className="card" key={character.id}>
+                <img
+                  src={character.images?.[0] ?? dummyImage}
+                  alt={`${character.name}の画像`}
+                  className="card-image"
+                ></img>
+                <div className="card-content">
+                  <h3 className="card-title">{character.name}</h3>
+                  <p className="card-description">
+                    {character.debut?.appearsIn ?? "情報なし"}
+                  </p>
+                  <div className="card-fotter">
+                    <span className="affiliation">
+                      {character.personal?.affiliation ?? "所属なし"}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </div>
   );
 }
 
